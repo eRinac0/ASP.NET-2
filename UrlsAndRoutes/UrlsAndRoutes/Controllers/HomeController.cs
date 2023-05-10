74936587f935fef45c50454b35555b770b3ea561
Vladik <vladkramarenko53@gmail.com>
2023-05-10 21:00:12 +0300

    example 3

---
+++ UrlsAndRoutes/UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace UrlsAndRoutes.AdditionalController
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "AdditionalController.Home";
+            ViewBag.Action = "Index";
+            return View("ActionName");
+        }
+        public ViewResult MyActionMethod()
+        {
+            string myActionUrl = Url.Action("Index", new { id = "MyID" });
+            string myRouteUrl = Url.RouteUrl(new { controller = "Home", action = "Index" });
+
+            // делать что-нибудь с URL
+
+            return View();
+        }
+    }
+}
